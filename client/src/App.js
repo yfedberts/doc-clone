07b2b507aeaf404aca28ea59b1f2e3c6d3c644a2
Y. F. Edbert Santoso <yfedbertsantoso@gmail.com>
2021-05-04 22:23:20 +0700

    Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import TextEditor from "./TextEditor";
+import {ChakraProvider} from '@chakra-ui/react';
 import {
   BrowserRouter as Router,
   Switch,
@@ -7,23 +8,27 @@ import {
 } from 'react-router-dom';
 import {v4 as uuidV4} from 'uuid';
 import Home from "./Home/Home";
+import Navbar from "./Layout/Navbar";
 
 function App() {
 
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Home/>
-        </Route>
-        <Route exact path="/documents">
-          <Redirect to={`/documents/${uuidV4()}`}/>
-        </Route>
-        <Route path="/documents/:id">
-          <TextEditor/>
-        </Route>
-      </Switch>
-    </Router>
+    <ChakraProvider>
+      <Router>
+      <Navbar/>
+        <Switch>
+          <Route exact path="/">
+            <Home/>
+          </Route>
+          <Route exact path="/documents">
+            <Redirect to={`/documents/${uuidV4()}`}/>
+          </Route>
+          <Route path="/documents/:id">
+            <TextEditor/>
+          </Route>
+        </Switch>
+      </Router>
+    </ChakraProvider>
   );
 }
 
--- client/src/Home/Home.js
+++ client/src/Home/Home.js
@@ -1,10 +1,24 @@
+import { Box, Flex, Grid, Spacer } from '@chakra-ui/layout';
 import React from 'react';
 
 function Home(props) {
     return (
-        <div>
-            <h1>Hello Wordl!</h1>
-        </div>
+        <>
+        <Flex>
+            <Box p="4" h="100vh" w="100vw" bg="gray.700"/>
+            <Spacer/>
+        </Flex>
+        <Flex>
+            
+            <Box p="4" h="100vh" w="100%" bg="gray.700">
+                Box 1
+            </Box>
+            <Spacer />
+            <Box p="4" h="100vh" w="60%" bg="gray.900">
+                Box 2
+            </Box>
+        </Flex>
+        </>
     );
 }
 
